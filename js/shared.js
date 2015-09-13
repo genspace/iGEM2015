8c5e92f5cfa2f37fc422a8364259787685dd6b62
blacki <blacki@gmail.com>
2015-09-13 19:53:17 -0400

    fixed positioning bug

--- js/shared.js
+++ js/shared.js
@@ -10,9 +10,9 @@ function size () {
   img.style({
     width:window.innerWidth+'px',
   })
-  content.style({
-    height:img.node().getBoundingClientRect().height,
-  })
+  // content.style({
+  //   height:img.node().getBoundingClientRect().height,
+  // })
 }
 
 d3.select(window).on('resize.index',size)
